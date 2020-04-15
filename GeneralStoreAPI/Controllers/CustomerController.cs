834767a121b8aa084abdddc6571bd79ad41739a2
d-spangler <61887496+d-spangler@users.noreply.github.com>
2020-04-15 14:00:00 -0400

    added general store API

---
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using GeneralStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeneralStoreAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        [HttpPost]//CREATE
+        public async Task<IHttpActionResult> Post(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+        
+        //GetAll - Return
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+
+        //GetById - Return
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            if(customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        //Update
+        [HttpPut]
+        public async Task<IHttpActionResult> Put([FromUri] int id, [FromBody] Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(id != model.Id)
+                {
+                    return BadRequest("Customer Id mismatch");
+                }
+                _context.Entry(model).State = EntityState.Modified;//This line is us updating our model.
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+
+
+    }
+}
---
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))] //Intoduction to foreign key, accessing other databases, potential to be involved in multiple transactions
+        public virtual Product Product { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [ForeignKey(nameof(CustomerId))]
+        public virtual Customer Customer { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfTransaction { get; set; }
+        
+    }
+}
